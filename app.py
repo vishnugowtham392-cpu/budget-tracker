a08472d36d820a4bc1d4faa14a05de4b54f2a66b
Vishnugowtham <yourgmail@gmail.com>
2026-06-23 09:29:36 +0530

    Renter fix windows compatibility

--- app.py
+++ app.py
@@ -344,5 +344,8 @@ def delete(id):
     return redirect('/')
 
 # ================= RUN =================
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
