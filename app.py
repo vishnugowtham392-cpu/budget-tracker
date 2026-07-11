c3095b935b1c265ecf4f7fc1cff135ec539ab878
Vishnugowtham <yourgmail@gmail.com>
2026-07-11 21:07:35 +0530

    Fixed Render deployment configuration

--- app.py
+++ app.py
@@ -1276,5 +1276,5 @@ def delete(id):
 
 # ================= RUN =================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
